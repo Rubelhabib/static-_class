877cd7ee571a52913ea6f3a3c867abe0b62e3310
Rubelhabib <rubelhabib007@gmail.com>
2024-03-16 15:49:10 +0600

    Add project files.

---
+++ static _class/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace static__class
+{
+    internal class Program
+    {
+        static class Product
+        {
+            public static int ProductId;
+            public static string ProductName;
+            public static string ProductDescription;
+            public static int ProductPrice;
+
+            static Product()
+            {
+                ProductId = 201;
+                ProductName = "Volvo";
+                ProductDescription = "Good Product";
+                ProductPrice = 500;
+            }
+            public static void productDetails()
+            {
+                Console.WriteLine("Product Id = {0}", ProductId);
+                Console.WriteLine("Product name = {0}", ProductName);
+                Console.WriteLine("Product description = {0}", ProductDescription);
+                Console.WriteLine("Product price = {0}", ProductPrice);
+            }
+            public static void productDiscount()
+            {
+                int d_amount = ProductPrice / 5;
+                Console.WriteLine("Your discount price is = {0}", d_amount);
+                Console.WriteLine("Total cost price is = {0}", ProductPrice - d_amount);
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            //Product.getProducetDetails();
+            //Product.getDiscount();
+
+            Product.productDetails();
+            Product.productDiscount();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ static _class/staticclass.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace static__class
+{
+    static class Product
+    {
+        public static int ProductId;
+        public static string ProductName;
+        public static int ProductPrice;
+
+        static Product()
+        {
+            ProductId = 101;
+            ProductName = "Audi";
+            ProductPrice = 6000;
+        }
+
+        public static void getProducetDetails()
+        {
+            Console.WriteLine($"Product id is: {ProductId} \n Product name is: {ProductName} \n Product price is : {ProductPrice}");
+        }
+        
+        public static void getDiscount()
+        {
+            int d_Amount = ProductPrice / 5;
+            Console.WriteLine("Your discount price is: = {0}",d_Amount);
+            Console.WriteLine("Total cost product is : = {0}",(ProductPrice - d_Amount));
+        }
+            
+            
+    }
+}
